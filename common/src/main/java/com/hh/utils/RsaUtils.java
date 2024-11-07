f8f38bdf07dffd82148740d0635433f889555562
fushaohui <wanghaiyong@semir.com>
2024-11-07 16:17:34 +0800

    优化RSA工具类

--- common/src/main/java/com/hh/utils/RsaUtils.java
+++ common/src/main/java/com/hh/utils/RsaUtils.java
@@ -11,30 +11,6 @@ import java.util.Base64;
 public class RsaUtils {
 
     /**
-     * 从文件中读取公钥
-     *
-     * @param filename 公钥保存路径，相对于classpath
-     * @return 公钥对象
-     * @throws Exception
-     */
-    public static PublicKey getPublicKey(String filename) throws Exception {
-        byte[] bytes = readFile(filename);
-        return getPublicKey(bytes);
-    }
-
-    /**
-     * 从文件中读取密钥
-     *
-     * @param filename 私钥保存路径，相对于classpath
-     * @return 私钥对象
-     * @throws Exception
-     */
-    public static PrivateKey getPrivateKey(String filename) throws Exception {
-        byte[] bytes = readFile(filename);
-        return getPrivateKey(bytes);
-    }
-
-    /**
      * 获取公钥
      *
      * @param bytes 公钥的字节形式
@@ -42,9 +18,6 @@ public class RsaUtils {
      * @throws Exception
      */
     public static PublicKey getPublicKey(byte[] bytes) throws Exception {
-//        X509EncodedKeySpec spec = new X509EncodedKeySpec(bytes);
-//        KeyFactory factory = KeyFactory.getInstance("RSA");
-//        return factory.generatePublic(spec);
         String key = new String(bytes)
                 .replace("-----BEGIN PUBLIC KEY-----", "")
                 .replace("-----END PUBLIC KEY-----", "")
@@ -53,6 +26,7 @@ public class RsaUtils {
         X509EncodedKeySpec spec = new X509EncodedKeySpec(decodedKey);
         KeyFactory factory = KeyFactory.getInstance("RSA");
         return factory.generatePublic(spec);
+
     }
 
     /**
@@ -63,9 +37,6 @@ public class RsaUtils {
      * @throws Exception
      */
     public static PrivateKey getPrivateKey(byte[] bytes) throws Exception {
-//        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(bytes);
-//        KeyFactory factory = KeyFactory.getInstance("RSA");
-//        return factory.generatePrivate(spec);
         String key = new String(bytes)
                 .replace("-----BEGIN PRIVATE KEY-----", "")
                 .replace("-----END PRIVATE KEY-----", "")
@@ -74,6 +45,7 @@ public class RsaUtils {
         PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decodedKey);
         KeyFactory factory = KeyFactory.getInstance("RSA");
         return factory.generatePrivate(spec);
+
     }
 
     /**
@@ -95,10 +67,6 @@ public class RsaUtils {
         // 获取私钥并写出
         byte[] privateKeyBytes = keyPair.getPrivate().getEncoded();
         writeFile(privateKeyFilename, privateKeyBytes);
-    }
-
-    private static byte[] readFile(String fileName) throws Exception {
-        return Files.readAllBytes(new File(fileName).toPath());
     }
 
     private static void writeFile(String destPath, byte[] bytes) throws IOException {
