3e55e5ac367dd90625fb8e042f577bc87a58ce74
fushaohui <wanghaiyong@semir.com>
2024-11-13 17:58:35 +0800

    修改默认生成验证码图片

--- service/user-service/src/main/java/com/hh/user/config/CaptchaConfig.java
+++ service/user-service/src/main/java/com/hh/user/config/CaptchaConfig.java
@@ -17,42 +17,37 @@ public class CaptchaConfig {
 
     @Bean
     public DefaultKaptcha captchaProducer() {
-        DefaultKaptcha kaptcha = new DefaultKaptcha();
+        //验证码生成器
+        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+        //配置
         Properties properties = new Properties();
-
-        // 设置边框颜色
-        properties.setProperty("kaptcha.border", "yes");
-        properties.setProperty("kaptcha.border.color", "105,179,90");  // 边框颜色
-
-        // 设置图片大小
-        properties.setProperty("kaptcha.image.width", "160");
-        properties.setProperty("kaptcha.image.height", "60");
-
-        // 设置文本颜色（随机色）
-        properties.setProperty("kaptcha.textproducer.font.color", "blue");  // 字体颜色
-        properties.setProperty("kaptcha.textproducer.font.size", "40");
-
-        // 设置验证码字符长度
-        properties.setProperty("kaptcha.textproducer.char.length", "5");
-
-        // 设置字符间距
-        properties.setProperty("kaptcha.textproducer.char.space", "5");
-
-        // 设置背景色（渐变效果）
-        properties.setProperty("kaptcha.background.clear.from", "lightGray");
-        properties.setProperty("kaptcha.background.clear.to", "white");
-
-        // 设置干扰线颜色
-        properties.setProperty("kaptcha.noise.color", "red");
-
-        // 设置字体
-        properties.setProperty("kaptcha.textproducer.font.names", "Arial, Courier, Times New Roman");
-
-        // 设置会话key（验证时使用）
-        properties.setProperty("kaptcha.session.key", "captchaCode");
-
-        kaptcha.setConfig(new Config(properties));
-        return kaptcha;
+        //是否有边框
+        properties.setProperty("kaptcha.border", "no");
+        //设置边框颜色
+        properties.setProperty("kaptcha.border.color", "105,179,90");
+        //边框粗细度，默认为1
+        // properties.setProperty("kaptcha.border.thickness","1");
+        //验证码
+        properties.setProperty("kaptcha.session.key", "code");
+        //验证码文本字符颜色 默认为黑色
+        properties.setProperty("kaptcha.textproducer.font.color", "blue");
+        //设置字体样式
+        properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅 黑");
+        //字体大小，默认40
+        properties.setProperty("kaptcha.textproducer.font.size", "30");
+        //验证码文本字符内容范围 默认为abced2345678gfynmnpwx
+        // properties.setProperty("kaptcha.textproducer.char.string", "");
+        //字符长度，默认为5
+        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        //字符间距 默认为2
+        properties.setProperty("kaptcha.textproducer.char.space", "4");
+        //验证码图片宽度 默认为200
+        properties.setProperty("kaptcha.image.width", "100");
+        //验证码图片高度 默认为40
+        properties.setProperty("kaptcha.image.height", "40");
+        Config config = new Config(properties);
+        defaultKaptcha.setConfig(config);
+        return defaultKaptcha;
     }
 
 }
