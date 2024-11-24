a58b842541505193e51e8bd5a05466ece0298661
fushaohui <wanghaiyong@semir.com>
2024-11-24 17:33:47 +0800

    完善代码生成配置

---
+++ code-generator/src/main/java/com/hh/generator/enums/GeneratorEnum.java
@@ -0,0 +1,13 @@
+package com.hh.generator.enums;
+
+/**
+ * @author huihui
+ * @date 2024/11/24 17:31
+ * @description GeneratorEnum
+ */
+public enum GeneratorEnum {
+
+    ENTITY,
+    INFO,
+
+}
--- code-generator/src/main/java/com/hh/generator/properties/GeneratorProperties.java
+++ code-generator/src/main/java/com/hh/generator/properties/GeneratorProperties.java
@@ -1,8 +1,11 @@
 package com.hh.generator.properties;
 
+import com.hh.generator.enums.GeneratorEnum;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author huihui
@@ -16,14 +19,33 @@ public class GeneratorProperties {
 
     private Datasource datasource;
 
+    private Package packageConfig;
+
+    private Generator generator;
+
     @Data
     public static class Datasource {
 
         private String url;
-
         private String username;
-
         private String password;
+
+    }
+
+    @Data
+    public static class Package {
+
+        private String modalName;
+        private String basePaceage;
+
+    }
+
+    @Data
+    public static class Generator {
+
+        private List<String> tableName;
+        private String tablePrefix;
+        private List<GeneratorEnum> code;
 
     }
 
